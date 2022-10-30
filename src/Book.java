e6dd142d9b3601689fb460cc851122986feaa8fc
Madina <madina-khat@yandex.ru>
2022-10-30 21:48:20 +0300

    1

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,18 @@
-public class Main {
+import java.io.OptionalDataException;
+import java.sql.SQLOutput;
+
+public class Book {
     public static void main(String[] args) {
-        String nameBook = "Book";
-        String nameAuthor = "Author";
-        String year = "year";
+        String nameBook = "Book ";
+        String nameAuthor = "Author ";
+        int year = 2022;
+
+        System.out.println("Имя - " + nameBook + "Автор - " + nameAuthor + "Год публикации " + year);
+        }
 
 
             }
 
 
-        }
+
 
